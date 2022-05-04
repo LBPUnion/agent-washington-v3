bd5182d6039bbc3bd3e54c8f116348837a52cf22
Alkaline Thunder <alkalinethunder@gmail.com>
2022-05-04 18:33:24 -0400

    I PRAY TO GOD that this works.

---
+++ src/LBPUnion.ServerReminders/Commands/AddGuildReminder.cs
@@ -0,0 +1,90 @@
+using Discord;
+using Discord.WebSocket;
+using LBPUnion.AgentWashington.Core;
+
+namespace LBPUnion.ServerReminders.Commands;
+
+public class AddGuildReminder : Command
+{
+    public override string Name => "addReminder";
+    public override string Description => "Add a recurring server reminder.";
+
+    public override IEnumerable<Option> Options
+    {
+        get
+        {
+            yield return new Option("dayOfWeek", OptionType.Integer, "Day of week - value between 1 and 7", true);
+            yield return new Option("hour", OptionType.Integer, "Hour of day, from 0-23, all times are UTC", true);
+            yield return new Option("channel", OptionType.Integer, "Where should the reminder be posted?", true);
+            yield return new Option("frequency", OptionType.Integer, "How many weeks between each reminder?", true);
+            yield return new Option("message", OptionType.String, "What should be said?", true);
+        }
+    }
+
+    protected override async Task OnHandle()
+    {
+        var text = GetArgument<string>("message");
+        var weeks = GetArgument<long>("frequency");
+        var channelId = GetArgument<long>("channel");
+        var hour = GetArgument<long>("hour");
+        var day = GetArgument<long>("dayOfWeek");
+        var reminderManager = Modules.GetModule<ServerRemindersPlugin>();
+        var guild = this.Guild;
+
+        var embedBuilder = new EmbedBuilder();
+
+        var channel = guild.GetTextChannel((ulong) channelId);
+
+        if (channel == null)
+        {
+            embedBuilder
+                .WithTitle("Cannot add reminder")
+                .WithDescription("The specified text channel was not found on this server.")
+                .WithColor(0xf71b1bu);
+
+            RespondWithEmbed(embedBuilder.Build());
+            return;
+        }
+
+        if (day < 1 || day > 7)
+        {
+            embedBuilder
+                .WithTitle("Cannot add reminder")
+                .WithDescription("The specified day of week must be a value between 1 and 7.")
+                .WithColor(0xf71b1bu);
+
+            RespondWithEmbed(embedBuilder.Build());
+            return;
+        }
+
+        if (hour < 0 || hour > 23)
+        {
+            embedBuilder
+                .WithTitle("Cannot add reminder")
+                .WithDescription("The specified hour must be a value between 0 and 23.")
+                .WithColor(0xf71b1bu);
+
+            RespondWithEmbed(embedBuilder.Build());
+            return;
+        }
+
+        var reminder = new ServerReminder
+        {
+            ChannelId = (ulong) channelId,
+            GuildId = guild.Id,
+            Text = text,
+            DayOfWeek = (DayOfWeek) (day - 1),
+            WeeksBetweenUpdate = (int) weeks,
+            Hour = (int) hour
+        };
+
+        reminderManager.AddReminder(reminder);
+        
+        embedBuilder
+            .WithTitle("Success!")
+            .WithDescription("Server reminder has been created!")
+            .WithColor(0x1baaf7u);
+
+        RespondWithEmbed(embedBuilder.Build());
+    }
+}
---
+++ src/LBPUnion.ServerReminders/ServerRemindersPlugin.cs
@@ -0,0 +1,150 @@
+﻿using Discord.WebSocket;
+using LBPUnion.AgentWashington.Core;
+using LBPUnion.AgentWashington.Core.Logging;
+using LBPUnion.AgentWashington.Core.Persistence;
+using LBPUnion.AgentWashington.Core.Plugins;
+using LBPUnion.AgentWashington.Core.Settings;
+using LBPUnion.ServerReminders.Commands;
+
+namespace LBPUnion.ServerReminders;
+
+[Plugin]
+public class ServerRemindersPlugin : BotModule
+{
+    private DatabaseManager database;
+    private SettingsManager settingsManager;
+    private CommandManager commandManager;
+    private DiscordBot bot;
+    private readonly List<ServerReminder> reminders = new List<ServerReminder>();
+
+    protected override void Init()
+    {
+        settingsManager = this.Modules.GetModule<SettingsManager>();
+        database = this.Modules.GetModule<DatabaseManager>();
+        commandManager = this.Modules.GetModule<CommandManager>();
+        bot = this.Modules.GetModule<DiscordBot>();
+
+        RestoreState();
+
+        commandManager.RegisterCommand<AddGuildReminder>();
+    }
+
+    protected override void BeforeInit()
+    {
+        Logger.Log("ServerReminders plugin is initializing");
+        base.BeforeInit();
+    }
+
+    protected override void OnTick(UpdateInterval interval)
+    {
+        var currentDate = DateTime.UtcNow;
+
+        foreach (var reminder in reminders)
+        {
+            if (reminder.DayOfWeek == currentDate.DayOfWeek && reminder.Hour == currentDate.Hour)
+            {
+                var hasHitMaxWeeks = reminder.WeeksSinceLastUpdate >= reminder.WeeksBetweenUpdate;
+                if (hasHitMaxWeeks)
+                    reminder.WeeksSinceLastUpdate = 0;
+                else
+                    reminder.WeeksSinceLastUpdate++;
+
+                if (hasHitMaxWeeks)
+                    PostReminder(reminder);
+
+                SaveState();
+            }
+        }   
+    }
+
+    public void AddReminder(ServerReminder reminder)
+    {
+        this.reminders.Add(reminder);
+        SaveState();
+    }
+    
+    private void RestoreState()
+    {
+        Logger.Log("Restoring server reminder state from database...");
+
+        database.OpenDatabase((db) =>
+        {
+            reminders.Clear();
+            var dbReminders = db.GetCollection<ServerReminder>(nameof(ServerReminder));
+            foreach (var reminder in dbReminders.FindAll())
+            {
+                Logger.Log($"{reminder.Id} loaded.");
+                reminders.Add(reminder);
+            }
+        });
+    }
+
+    private void PostReminder(ServerReminder reminder)
+    {
+        var guilds = bot.GetGuilds();
+        var guild = guilds.FirstOrDefault(x => x.Id == reminder.GuildId);
+        if (guild == null)
+            return;
+
+        var textChannel = guild.GetChannel(reminder.ChannelId) as SocketTextChannel;
+        if (textChannel == null)
+            return;
+
+        try
+        {
+            textChannel.SendMessageAsync(reminder.Text).GetAwaiter().GetResult();
+        }
+        catch (Exception ex)
+        {
+            Logger.Log(
+                $"Couldn't post server reminder {reminder.Id} to guild {reminder.GuildId}, channel {reminder.ChannelId}: An exception was thrown by Discord's API. {ex.Message}",
+                LogLevel.Warning);
+        }
+    }
+    
+    private void SaveState()
+    {
+        this.database.OpenDatabase(SaveStateInternal);
+    }
+
+    private void SaveStateInternal(Database db)
+    {
+        var reminderCollection = db.GetCollection<ServerReminder>(nameof(ServerReminder));
+
+        Logger.Log("Saving server reminder state to Database...");
+
+        foreach (var reminder in this.reminders)
+        {
+            if (reminderCollection.FindOne(x => x.Id == reminder.Id) != null)
+            {
+                Logger.Log($"{reminder.Id} updated in database");
+                reminderCollection.Update(reminder);
+            }
+            else
+            {
+                Logger.Log($"{reminder.Id} saved to database");
+                reminderCollection.Insert(reminder);
+            }
+        }
+
+        var deleted = reminderCollection.Find(x => this.reminders.All(y => y.Id != x.Id)).ToArray();
+
+        foreach (var deletedReminder in deleted)
+        {
+            Logger.Log($"{deletedReminder.Id} removed from database");
+            reminderCollection.Delete(deletedReminder.Id);
+        }
+    }
+}
+
+public class ServerReminder : IDatabaseObject
+{
+    public int WeeksSinceLastUpdate { get; set; }
+    public int WeeksBetweenUpdate { get; set; }
+    public int Hour { get; set; }
+    public DayOfWeek DayOfWeek { get; set; }
+    public ulong GuildId { get; set; }
+    public ulong ChannelId { get; set; }
+    public string Text { get; set; }
+    public Guid Id { get; set; }
+}
